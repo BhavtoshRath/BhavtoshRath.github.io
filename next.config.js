5c4caa9c5750a94c70822e8ae83ea12c2be2e7b5
BR <33324272+BhavtoshRath@users.noreply.github.com>
2025-04-23 18:59:47 -0500

    test

--- next.config.js
+++ next.config.js
@@ -1,8 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  basePath: '/bhavtoshrath.github.io',
-  assetPrefix: '/bhavtoshrath.github.io',
   images: {
     unoptimized: true,
   },
